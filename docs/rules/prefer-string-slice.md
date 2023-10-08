defd5f0f9273051df6a238fb16371368ea9e10b4
so1ve <i@mk1.io>
2023-10-08 22:23:06 +0800

    docs: update

---
+++ docs/rules/prefer-string-slice.md
@@ -0,0 +1,7 @@
+# Prefer `String#slice()` over `String#substr()` and `String#substring()` (`pegasus/prefer-string-slice`)
+
+🔧 This rule is automatically fixable by the [`--fix` CLI option](https://eslint.org/docs/latest/user-guide/command-line-interface#--fix).
+
+💭 This rule requires type information.
+
+<!-- end auto-generated rule header -->
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import type { TSESLint } from "@typescript-eslint/utils";
 
 import preferTopLevelAwait from "./rules/prefer-array-flat-map";
+import preferStringSlice from "./rules/prefer-string-slice";
 
 export default {
 	rules: {
 		"prefer-top-level-await": preferTopLevelAwait,
+		"prefer-string-slice": preferStringSlice,
 	} satisfies Record<string, TSESLint.RuleModule<string, unknown[]>>, // Fixes "The inferred type of 'default' cannot be named without a reference to..."
 };
